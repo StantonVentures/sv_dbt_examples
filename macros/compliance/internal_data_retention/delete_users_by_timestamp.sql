f014e2109732fa8e06bab871afef843a889dd114
Alison (she/her) <alison@stantonventures.com>
2024-03-01 19:25:40 +0000

    it helps when you commit the changes

--- macros/compliance/internal_data_retention/delete_users_by_timestamp.sql
+++ macros/compliance/internal_data_retention/delete_users_by_timestamp.sql
@@ -3,13 +3,6 @@
 
 DELETE FROM prepared_app_users WHERE src_created_at < {{cutoff_timestamp}}; 
 
-UPDATE snapshot_app_users 
-  SET dbt_valid_from = {{cutoff_timestamp}}
-    , dbt_updated_at = {{cutoff_timestamp}} 
-  WHERE dbt_valid_from < {{cutoff_timestamp}};
-
-DELETE FROM snapshot_app_users WHERE dbt_valid_from < {{cutoff_timestamp}};
-
-DELETE FROM snapshot_app_users WHERE updated_at < {{cutoff_timestamp}};
+DELETE FROM snapshot_app_users WHERE created_at < {{cutoff_timestamp}};
 
 {% endmacro %}
